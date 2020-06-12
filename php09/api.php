48d93c9fffbacd033feb07dfb3c6ef2c416e68ed
long-hai <long-hai@applh.com>
2020-06-12 14:48:30 +0200

    doc++

--- php09/api.php
+++ php09/api.php
@@ -2,9 +2,11 @@
 
 require_once "php/controller/Ajax.php";
 require_once "php/controller/Controller.php";
+require_once "php/model/Model.php";
+
+// AJOUTER LES API POUR LES FORMULAIRES
 require_once "php/controller/ApiNewsletter.php";
 require_once "php/controller/ApiContact.php";
-require_once "php/model/Model.php";
 
 Ajax::traiterFormulaire();
 
