12f58ee54f3774a9ee34da773f14d2cdfbb84d9c
long-hai <long-hai@applh.com>
2020-06-17 16:15:15 +0200

    ajout redirection apres logout

--- php12/php/view/section-logout.php
+++ php12/php/view/section-logout.php
@@ -3,6 +3,11 @@
         <h2>DECONNEXION</h2>
         <script>
 // SUPPRIMER LA CLE DANS sessionStorage
-sessionStorage.removeItem('cleAPI');            
+sessionStorage.removeItem('cleAPI');   
+
+// DELAI ET REDIRECTION
+setTimeout(function() {
+    location.href = 'login.php';
+}, 2000);
         </script>
     </section>
