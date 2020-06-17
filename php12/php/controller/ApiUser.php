40c212aca53ba7efb7015354ccc2f1ef4659f86a
long-hai <long-hai@applh.com>
2020-06-17 10:18:30 +0200

    ajout code traitement ApiUser::installer

--- php12/php/controller/ApiUser.php
+++ php12/php/controller/ApiUser.php
@@ -7,5 +7,20 @@ class ApiUser
     {
         // DEBUG
         echo "ApiUser::installer";
+        $tabAssoToken =
+        [
+            "email"             => Controller::filtrer("email"),
+            "login"             => Controller::filtrer("login"),
+            "password"          => Controller::filtrer("password"),
+            // COMPLETER LES COLONNES MANQUANTES
+            "level"             => 100,                 // level = 100 POUR ADMIN
+            "dateCreation"      => date("Y-m-d H:i:s"), // FORMAT DATETIME 2020-06-15 11:07:23
+        ];
+
+        if (Controller::isOK())
+        {
+            Model::insert("user", $tabAssoToken);
+            Controller::$tabAssoJson["listeUser"] = Model::read("user");
+        }
     }
 }
